90e228fcfd58ff491635d1ed196765c29804466f
myh1000 <myh1000@gmail.com>
2016-07-09 20:06:49 -0700

    done

--- face.py
+++ face.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import ast, json
+from bottle import route, run, template, static_file, get, post, request
 
 # Data sets
 IRIS_TRAINING = "training.csv"
@@ -23,7 +25,31 @@ accuracy_score = classifier.evaluate(x=x_test, y=y_test)["accuracy"]
 print('Accuracy: {0:f}'.format(accuracy_score))
 
 # Classify two new flower samples.
-new_samples = np.array(
-    [[0, 0, 0, 0, 0], [0, 0.2, 0, 1, 0]], dtype=float)
-y = classifier.predict(new_samples)
-print ('Predictions: {}'.format(str(y)))
+@route('/')
+def index():
+    return "same"
+
+@post('/classify') # or @route('/login', method='POST')
+def classify():
+    message = request.forms.get('message')
+    print message
+    data = json.loads(message)
+    print data
+    attributes = data[0]['faceAttributes']
+    face = attributes['facialHair']
+    glasses = attributes['glasses']
+    gender = attributes['gender']
+    if gender == "male":
+        gender = 0
+    else:
+        gender = 1
+    if glasses == "NoGlasses":
+        glasses = 0
+    else:
+        glasses = 1
+    array = np.array([face['moustache'], face['beard'], face['sideburns'], glasses, gender], dtype=float)
+    y = classifier.predict(array)
+    print ('Predictions: {}'.format(str(y)))
+    return str(y)
+
+run(host='localhost', port=8080)
